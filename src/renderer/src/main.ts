fb64b0bb66de5fbaafcafbde6f7995ab785bcfdf
zzq <1030907690@qq.com>
2025-05-10 16:25:35 +0800

    feat:vue-router

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -6,7 +6,13 @@ import App from './App.vue'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import router from './router'
+
+
 const app = createApp(App)
 app.use(ElementPlus)
+
+// 配置路由
+app.use(router)
 
 app.mount('#app')
